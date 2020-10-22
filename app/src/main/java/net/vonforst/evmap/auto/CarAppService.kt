2e4db4c3c30a69d0c6c84209d761f991319b34ec
johan12345 <johan.forstner@gmail.com>
2020-10-22 08:49:12 +0200

    Add CarAppService

---
+++ app/src/main/java/net/vonforst/evmap/auto/CarAppService.kt
@@ -0,0 +1,29 @@
+package net.vonforst.evmap.auto
+
+import android.content.Intent
+import com.google.android.libraries.car.app.CarContext
+import com.google.android.libraries.car.app.Screen
+import com.google.android.libraries.car.app.model.Pane
+import com.google.android.libraries.car.app.model.PaneTemplate
+import com.google.android.libraries.car.app.model.Row
+import com.google.android.libraries.car.app.model.Template
+
+
+class CarAppService : com.google.android.libraries.car.app.CarAppService() {
+    override fun onCreateScreen(intent: Intent): Screen {
+        return HelloWorldScreen(carContext)
+    }
+}
+
+class HelloWorldScreen(ctx: CarContext) : Screen(ctx) {
+    override fun getTemplate(): Template {
+        val pane: Pane = Pane.builder()
+            .addRow(
+                Row.builder()
+                    .setTitle("Hello world!")
+                    .build()
+            )
+            .build()
+        return PaneTemplate.builder(pane).build()
+    }
+}
