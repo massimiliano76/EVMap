b9135546ae27939edfd0fb85a401a438e08dc848
johan12345 <johan.forstner@gmail.com>
2020-10-27 09:02:13 +0100

    draw a google map in the background

--- app/src/google/java/net/vonforst/evmap/auto/CarAppService.kt
+++ app/src/google/java/net/vonforst/evmap/auto/CarAppService.kt
@@ -1,20 +1,24 @@
 package net.vonforst.evmap.auto
 
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.Rect
 import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.view.Surface
+import android.view.View
 import com.google.android.libraries.car.app.*
 import com.google.android.libraries.car.app.model.*
 import com.google.android.libraries.car.app.model.Distance.UNIT_KILOMETERS
 import com.google.android.libraries.car.app.navigation.model.PlaceListNavigationTemplate
+import com.google.android.libraries.maps.GoogleMap
+import com.google.android.libraries.maps.GoogleMapOptions
+import com.google.android.libraries.maps.MapView
 
 
 class CarAppService : com.google.android.libraries.car.app.CarAppService() {
     override fun onCreateScreen(intent: Intent): Screen {
-        carContext.getCarService(AppManager::class.java).setSurfaceListener(MapSurfaceListener())
+        carContext.getCarService(AppManager::class.java)
+            .setSurfaceListener(MapSurfaceListener(carContext))
         return MapScreen(carContext)
     }
 }
@@ -42,16 +46,40 @@ class MapScreen(ctx: CarContext) : Screen(ctx) {
     }
 }
 
-class MapSurfaceListener : SurfaceListener {
+class MapSurfaceListener(val carContext: CarContext) : SurfaceListener {
     private var surfaceContainer: SurfaceContainer? = null
     private var surface: Surface? = null
+    private var mapView: MapView? = null
+    private var map: GoogleMap? = null
 
     override fun onSurfaceAvailable(surfaceContainer: SurfaceContainer) {
         this.surfaceContainer = surfaceContainer
         this.surface = surfaceContainer.surface
+
+        this.map = null
+        val mapView = MapView(carContext, GoogleMapOptions().liteMode(true))
+        mapView.onCreate(null)
+        mapView.onStart()
+        mapView.onResume()
+
+        mapView.getMapAsync { map ->
+            map.setOnMapLoadedCallback {
+                this.map = map
+                renderFrame()
+            }
+        }
+
+        mapView.measure(
+            View.MeasureSpec.makeMeasureSpec(surfaceContainer.width, View.MeasureSpec.EXACTLY),
+            View.MeasureSpec.makeMeasureSpec(surfaceContainer.height, View.MeasureSpec.EXACTLY)
+        )
+        mapView.layout(0, 0, surfaceContainer.width, surfaceContainer.height)
+
+        this.mapView = mapView
     }
 
     override fun onStableAreaChanged(stableArea: Rect) {
+        mapView?.setPadding(stableArea.left, stableArea.top, stableArea.right, stableArea.bottom)
         renderFrame()
     }
 
@@ -61,18 +89,23 @@ class MapSurfaceListener : SurfaceListener {
 
     override fun onSurfaceDestroyed(surfaceContainer: SurfaceContainer) {
         surface = null
+        mapView?.onPause()
+        mapView?.onStop()
+        mapView?.onDestroy()
     }
 
     private fun renderFrame() {
         val s = surface
-        if (s == null || !s.isValid()) {
+        if (s == null || !s.isValid) {
             // Surface is not available, or has been destroyed, skip this frame.
             return;
         }
 
-        val canvas = s.lockCanvas(null)
-        canvas.drawColor(Color.LTGRAY)
-        s.unlockCanvasAndPost(canvas);
+        map?.snapshot {
+            val canvas = s.lockCanvas(null)
+            canvas.drawBitmap(it, 0f, 0f, null)
+            s.unlockCanvasAndPost(canvas);
+        }
     }
 
 }
